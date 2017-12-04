cc76e003e0362b81dee4ebf47ccdee1454b771fe
Lightshadow244 <richi.lightshadow@gmail.com>
2017-12-04 15:12:49 +0100

    elastic entfernt

--- ownmusicweb/elastic/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost
-
-
-# Register your models here.
-# Need to register my BlogPost so it shows up in the admin
-admin.site.register(BlogPost)
