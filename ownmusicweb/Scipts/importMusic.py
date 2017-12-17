835379e6bf19466229464bc3621269c097163188
Lightshadow244 <richi.lightshadow@gmail.com>
2017-12-17 10:01:22 +0100

    add script

--- ownmusicweb/Scipts/importMusic.py
+++ ownmusicweb/Scipts/importMusic.py
@@ -1,7 +1,9 @@
 # import music, infomations and the path into the database
-import sqlite3
+import sqlite3, eyed3
 conn=sqlite3.connect('../db.sqlite3')
 c = conn.cursor()
 
 c.execute('select * from player_album')
-print(c.fetchone())
+print(c.fetchall())
+
+#C:\Users\Richii\Music
