dfbf68708723448d1fc1770a6decad2aa3f1648a
Lightshadow244 <richi.lightshadow@gmail.com>
2018-02-09 16:14:30 +0100

    first REST Integration

--- ownmusicweb/ownmusicweb/settings.py
+++ ownmusicweb/ownmusicweb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,11 @@ STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, "static"),
     '/home/user/music/',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
