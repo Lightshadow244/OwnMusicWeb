5e3b502d7b0100c5a95b1152e555c547435e1d1e
Lightshadow244 <richi.lightshadow@gmail.com>
2018-04-09 16:52:30 +0200

    up

--- ownmusicweb/ownmusicweb/settings.py
+++ ownmusicweb/ownmusicweb/settings.py
@@ -57,21 +57,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ownmusicweb.urls'
 
-#TEMPLATES = [
-#    {
-#        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#        'DIRS': [],
-#        'APP_DIRS': True,
-#        'OPTIONS': {
-#            'context_processors': [
-#                'django.template.context_processors.debug',
-#                'django.template.context_processors.request',
-#                'django.contrib.auth.context_processors.auth',
-#                'django.contrib.messages.context_processors.messages',
-#            ],
-#        },
-#    },
-#]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'ownmusicweb.wsgi.application'
 
