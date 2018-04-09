37c918c676c973c083882e1b5a63a4fff3b3f37f
Lightshadow244 <richi.lightshadow@gmail.com>
2018-04-09 11:25:49 +0200

    up

--- ownmusicweb/ownmusicweb/settings.py
+++ ownmusicweb/ownmusicweb/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIRS = (
-    "/home/user/ownmusicweb/templates",
+    '/home/user/OwnMusicWeb/ownmusicweb/templates',
 )
 
 # Quick-start development settings - unsuitable for production
