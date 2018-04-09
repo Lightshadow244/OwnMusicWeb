1630a098639c4c83cb28ef296319c9fac85ab1da
Lightshadow244 <richi.lightshadow@gmail.com>
2018-04-09 17:16:18 +0200

    up

--- ownmusicweb/ownmusicweb/urls.py
+++ ownmusicweb/ownmusicweb/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'^', include('player.urls')),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^admin/', include(admin.site.urls)),
 ]
