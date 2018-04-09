6a680b1870b6111cc422f323a783f1c998491d99
Lightshadow244 <richi.lightshadow@gmail.com>
2018-04-09 11:15:42 +0200

    up

--- ownmusicweb/player/urls.py
+++ ownmusicweb/player/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import *
+from player.views import all_songs, song
 
 from . import views
 
 urlpatterns = [
-	url(r'^$', 'player.views.all_songs'),
-	url(r'?P<song_id>\d+)/$', 'player.views.song'),
+	url(r'^$', all_song, name='all_songs'),
+	url(r'?P<song_id>\d+)/$', song, name='song'),
 ]
