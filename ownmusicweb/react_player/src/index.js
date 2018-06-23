e94c671a58f8af5b8d90f51e500b79979b5172b3
Lightshadow244 <richi.lightshadow@gmail.com>
2018-06-23 15:24:07 +0200

    reactplayer

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -1,68 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ReactAudioPlayer from 'react-audio-player';
 
-class Square extends React.Component {
+class SongBoard extends React.Component {
+
+}
+
+class Player extends React.Component {
   render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+    return(
+      <ReactAudioPlayer
+        src="my_audio_file.ogg"
+        autoPlay
+        controls
+      />
+    )
   }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
+class Site extends React.Component {
   render() {
-    const status = 'Next player: X';
-
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      <div className="player">
+        <Player />
       </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+      <div className="song-board"
+        <SongBoard />
       </div>
-    );
+    )
   }
 }
-
-// ========================================
 
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
