6eeb0c8e43724b75cb0736dc0724109d2ef7351b
Lightshadow244 <richi.lightshadow@gmail.com>
2018-06-23 12:46:12 +0200

    react update

--- ownmusicweb/static/js/indexReact.jsx
+++ ownmusicweb/static/js/indexReact.jsx
@@ -1,12 +1,12 @@
 var SongComponent = React.createClass({
-  render: function( {
+  render: function () {
     var testStyle = { fontSize: '18px' }; // font-size in html
     return(
       <div style={testStyle}className="SongTabelle">
         This text is 18px tall
       </div>
     )
-  })
+  }
 
 });
 
