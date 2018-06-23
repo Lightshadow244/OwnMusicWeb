3678ab8b60eec13d4a2b129495a3111ec7f714e9
Lightshadow244 <richi.lightshadow@gmail.com>
2018-06-23 23:30:00 +0200

    songs

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -8,9 +8,13 @@ import axios from "axios";
 
 class AvailableDatalist extends React.Component {
   render() {
-    {/*return(<div>{this.props.songs[0]['name']}</div>)*/}
-    return(<div>{this.props.songs[0]['name']}</div>)
+    var songArray = []
+    for (var i = 0; i < this.props.songs.length; i++) {
+      songArray.push(
+        <div>{this.props.songs[i]['name']}</div>
+      )
   }
+  return({songArray})
 }
 
 AvailableDatalist.propTypes = {
