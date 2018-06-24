cdbf2123de82ff1934d15ecfd808fd2ec6fb0ae3
Lightshadow244 <richi.lightshadow@gmail.com>
2018-06-24 10:51:41 +0200

    songs

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -9,11 +9,11 @@ import axios from "axios";
 class AvailableDatalist extends React.Component {
   render() {
     console.log("props during render songs")
-    console.log(this.props)
+    console.log(this.props.songs)
     var ret = []
     if(this.props[0] != 0){
       ret = this.props.songs.map(c => {
-        
+
         <div> <div>c.songName[0]</div> <div>c.album</div> <div>c.author</div> <div>c.date</div> </div>
       })
     }else{
