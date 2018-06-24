7a45407e431cb4aa4595f0354f9af6dd5faf5971
Lightshadow244 <richi.lightshadow@gmail.com>
2018-06-24 11:02:42 +0200

    songs

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -13,7 +13,7 @@ class AvailableDatalist extends React.Component {
     var ret = []
     if(this.props.songs[0] != 0){
       ret = this.props.songs.map(c =>
-        <div> <div>c.songName[0]</div> <div>c.album</div> <div>c.author</div> <div>c.date</div> </div>
+        <div> <div>{c.songName[0]}</div> <div>{c.album}</div> <div>{c.author}</div> <div>{c.date}</div> </div>
       )
     }else{
       ret[0] = "test"
