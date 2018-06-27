20e50350fdd8e93dce4992bfe24369385b2627bb
Lightshadow244 <richi.lightshadow@gmail.com>
2018-06-27 17:46:28 +0200

    serializer.py

--- ownmusicweb/rest/serializers.py
+++ ownmusicweb/rest/serializers.py
@@ -3,20 +3,20 @@ from player.models import Album, Song
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'url', 'username', 'email', 'groups')
 
 
-class AlbumSerializer(serializers.HyperlinkedModelSerializer):
+class AlbumSerializer(serializers.ModelSerializer):
     songs = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Album
         fields = ('id', 'name', 'author', 'release_date', 'songs')
 
 class SongSerializer(serializers.ModelSerializer):
-    album= serializers.CharField(read_only=True)
+    album = serializers.CharField(read_only=True)
     class Meta:
         model = Song
         fields = ('id', 'name','album', 'change_date', 'audio_file')
