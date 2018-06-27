7f7322f83fd91adf31be9e3c45522691f5fcdec1
Lightshadow244 <richi.lightshadow@gmail.com>
2018-06-27 17:48:58 +0200

    serializer.py

--- ownmusicweb/rest/serializers.py
+++ ownmusicweb/rest/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    songs = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    songs = serializers.RelatedField(source='name', read_only=True)
     class Meta:
         model = Album
         fields = ('id', 'name', 'author', 'release_date', 'songs')
