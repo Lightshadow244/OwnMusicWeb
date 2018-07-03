1aee9170f69c9b4eb476a749dbc5ef994115cb3d
Lightshaodw244 <richi.lightshadow@gmail.com>
2018-07-03 13:41:40 +0200

    bootstrap

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -89,10 +89,14 @@ class SongBoard extends React.Component {
           <div className="directory">
 						<div className="btn-group btn-group-toggle" data-toggle="buttons">
 						  <label className="btn btn-secondary active">
-						    <input type="radio" name="options" id="option1" autocomplete="off" checked> Song
+						    <input type="radio" name="options" id="option1" autocomplete="off" checked>
+									Song
+								</input>
 						  </label>
 						  <label class="btn btn-secondary">
-						    <input type="radio" name="options" id="option2" autocomplete="off"> Album
+						    <input type="radio" name="options" id="option2" autocomplete="off">
+									Album
+								</input>
 						  </label>
 						</div>
           </div>
