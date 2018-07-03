e2581982eb3660815f5fc9e3520f86708c2df7ae
Lightshaodw244 <richi.lightshadow@gmail.com>
2018-07-03 14:18:37 +0200

    bootstrap

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -84,28 +84,28 @@ class SongBoard extends React.Component {
   render() {
 
     return(
-
-      <div className="table row">
-				<div className="col-lg-6">
-          <div className="directory">
-						<div className="btn-group btn-group-toggle" data-toggle="buttons">
-						  <label className="btn btn-secondary active">
-						    <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked />
-									Song
-						  </label>
-						  <label className="btn btn-secondary">
-						    <input type="radio" name="options" id="option2" autoComplete="off" />
-									Album
-						  </label>
-						</div>
-          </div>
-          <AvailableDatalist song_set={this.state.song_set}/> {/*songs={this.state.contacts}*/}
-				</div>
+      <div className="table container-fluid">
+        <div className=" table row">
+					<div className="col-lg-6">
+	          <div className="directory">
+							<div className="btn-group btn-group-toggle" data-toggle="buttons">
+							  <label className="btn btn-secondary active">
+							    <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked />
+										Song
+							  </label>
+							  <label className="btn btn-secondary">
+							    <input type="radio" name="options" id="option2" autoComplete="off" />
+										Album
+							  </label>
+							</div>
+	          </div>
+	          <AvailableDatalist song_set={this.state.song_set}/> {/*songs={this.state.contacts}*/}
+					</div>
+        </div>
+        <div className="playlist col-lg-6">
+					playlist
+        </div>
       </div>
-      <div className="playlist col-lg-6">
-				playlist
-      </div>
-
     )
   }
 }
@@ -126,10 +126,10 @@ class Site extends React.Component {
   render() {
     return (
       <div className="site">
-        <div className="player container-fluid">
+        <div className="player">
           <Player />
         </div>
-        <div className="container-fluid">
+        <div>
           <SongBoard />
         </div>
       </div>
