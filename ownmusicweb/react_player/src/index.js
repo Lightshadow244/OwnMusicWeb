ecddb6944b1cc6b506798906d7df573ec26a0015
Lightshaodw244 <richi.lightshadow@gmail.com>
2018-07-03 14:53:53 +0200

    bootstrap

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -29,7 +29,7 @@ class AvailableDatalist extends React.Component {
     console.log(ret)
 
     return(
-      <table>
+      <table className="table-striped">
         <tbody>
           <tr>
             <th>Songname</th>
@@ -88,16 +88,28 @@ class SongBoard extends React.Component {
         <div className="row">
 					<div className="col-lg-8 bg-primary">
 	          <div className="directory">
-							<div className="btn-group btn-group-toggle" data-toggle="buttons">
-							  <label className="btn btn-secondary active">
-							    <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked />
-										Song
-							  </label>
-							  <label className="btn btn-secondary">
-							    <input type="radio" name="options" id="option2" autoComplete="off" />
-										Album
-							  </label>
-							</div>
+							<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+							  <a class="navbar-brand" href="#">Navbar</a>
+							  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+							    <span class="navbar-toggler-icon"></span>
+							  </button>
+
+							  <div class="collapse navbar-collapse" id="navbarColor01">
+							    <ul class="navbar-nav mr-auto">
+							      <li class="nav-item active">
+							        <a class="nav-link" href="#">Songs <span class="sr-only">(current)</span></a>
+							      </li>
+							      <li class="nav-item">
+							        <a class="nav-link" href="#">Album</a>
+							      </li>
+
+							    </ul>
+							    <form class="form-inline my-2 my-lg-0">
+							      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+							      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+							    </form>
+							  </div>
+							</nav>
 	          </div>
 	          <AvailableDatalist song_set={this.state.song_set}/> {/*songs={this.state.contacts}*/}
 					</div>
