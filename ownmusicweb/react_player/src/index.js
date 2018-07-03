3451686867507450c416e68705f89e77c479e004
Lightshaodw244 <richi.lightshadow@gmail.com>
2018-07-03 14:37:39 +0200

    bootstrap

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+{//import 'bootstrap/dist/css/bootstrap.css';}
+import './bootstrap-lux.css';
 import './index.css';
 
 import PropTypes from "prop-types";
