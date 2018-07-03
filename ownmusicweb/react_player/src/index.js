a9b20a77d04579df74d8fe80fb26e4c47a8e2984
Lightshaodw244 <richi.lightshadow@gmail.com>
2018-07-03 14:57:32 +0200

    bootstrap

--- ownmusicweb/react_player/src/index.js
+++ ownmusicweb/react_player/src/index.js
@@ -89,7 +89,6 @@ class SongBoard extends React.Component {
 					<div className="col-lg-8 bg-primary">
 	          <div className="directory">
 							<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-							  <a className="navbar-brand" href="#">Navbar</a>
 							  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 							    <span className="navbar-toggler-icon"></span>
 							  </button>
